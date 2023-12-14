d50ce60dd26fb193010afb0d00b3aac308872c70
Jordan Witte <wittejm@gmail.com>
2023-12-13 16:43:09 -0800

    manually set port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ app.get('/hello', (_req :any, res :any) => {
   res.send('Hello, World222!');
 });
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
