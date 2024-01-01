829496847d6753ab305e676c39573c8bbb194f27
Jordan Witte <wittejm@gmail.com>
2024-01-01 14:34:03 -0800

    new blank route at /

--- backend/formfillservice/flask_app.py
+++ backend/formfillservice/flask_app.py
@@ -27,6 +27,10 @@ app.register_blueprint(oeci_scrape.bp)
 app.register_blueprint(oeci_login.bp)
 
 @app.route("/", defaults={"path": ""})
+def blank(): 
+    logger.info("called blank")
+    return "blank"
+
 @app.route("/<path:path>", methods=["GET", "HEAD", "OPTIONS"])
 def try_files_static_index(path):
     logger.info(f"serving file: {path}")
